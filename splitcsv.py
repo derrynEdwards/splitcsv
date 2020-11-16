fcfe9439e9aa7beb4a28f19c5fd7e0946faa3530
Derryn Edwards <derryn_93@live.com>
2020-11-16 09:48:43 -0600

    adds file info and comments

--- splitcsv.py
+++ splitcsv.py
@@ -1,20 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    File name           : web.py
+    Author              : Derryn Edwards
+    Date Created        : 2020/11/16
+    Date Last Modified  : 2020/11/16 09:48:21
+    Python Version      : 3.9
+"""
+# ==================================================================================================
+# IMPORTS
+# ==================================================================================================
 import csv
 import sys
 
 try:
+    # grabs arguments
     file_name = sys.argv[1]
     output_prefix = sys.argv[2]
     max_line = int(sys.argv[3])
+    # opens csv file
     csv_file = open(file_name, 'r')
     csv_data = list(csv.reader(csv_file, delimiter=','))
+    # line counters and mappers
     line_count = 0
     line_mapper = 0
     file_count = 1
+    # csv header
     header = csv_data[0]
 
     while line_mapper < (len(csv_data)-1):
         new_list = []
         new_list.append(header)
+        # opens new file
         new_file = open(output_prefix + '_' + str(file_count), 'w')
 
         for row in csv_data[line_mapper+1:]:
@@ -24,7 +40,7 @@ try:
                 line_mapper += 1
             else:
                 break
-
+        # writes into new file
         csv.writer(new_file).writerows(new_list)
         new_file.close()
         print(new_list)
